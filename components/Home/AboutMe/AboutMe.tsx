4fd27005a20ab1f7ab54f8edde1a4cc6ab4516bf
ADITYA BASANTIA <adityabasantia729@gmail.com>
2024-11-08 08:45:35 +0530

    image

--- components/Home/AboutMe/AboutMe.tsx
+++ components/Home/AboutMe/AboutMe.tsx
@@ -3,13 +3,15 @@ import Img from "../../../components/smallComp/image/Img";
 export default function AboutMe(props) {
   const technologies = [
     ["React.js", "JavaScript (ES6+)", "Tailwind CSS", "Express.js"],
-    ["Node.js", "Framer Motion", "Bootstrap","MongoDB"],
+    ["Node.js", "Framer Motion", "Bootstrap", "MongoDB"],
   ];
   const highlightWords = [
     "Hello!",
     "Aditya",
-    "software","engineer",
-    "full-stack", "developer",
+    "software",
+    "engineer",
+    "full-stack",
+    "developer",
     "2022",
     "backend",
     "HTML",
@@ -24,22 +26,26 @@ export default function AboutMe(props) {
     "high-quality",
     "positive impact",
   ];
-    // Function to highlight specific words in a given text
-    const highlightText = (text) => {
-      return text.split(' ').map((word, index) => {
-        const cleanedWord = word.replace(/[.,]/g, ''); // Clean punctuation for matching
-        if (highlightWords.includes(cleanedWord)) {
-          return (
-            <span key={index} className="text-AAsecondary">
-              {word}{' '}
-            </span>
-          );
-        }
-        return <span key={index}>{word} </span>;
-      });
-    };
+  // Function to highlight specific words in a given text
+  const highlightText = (text) => {
+    return text.split(" ").map((word, index) => {
+      const cleanedWord = word.replace(/[.,]/g, ""); // Clean punctuation for matching
+      if (highlightWords.includes(cleanedWord)) {
+        return (
+          <span key={index} className="text-AAsecondary">
+            {word}{" "}
+          </span>
+        );
+      }
+      return <span key={index}>{word} </span>;
+    });
+  };
   return (
-    <div id="aboutSection" data-aos="fade-up" className="snap-start flex flex-col  items-center py-20 bg-AAprimary">
+    <div
+      id="aboutSection"
+      data-aos="fade-up"
+      className="snap-start flex flex-col  items-center py-20 bg-AAprimary"
+    >
       {/* // ? 0.1 About Me */}
       <div
         className="flex flex-col space-y-8 px-4 sm:px-0 w-full sm:w-[500px] 
@@ -47,8 +53,15 @@ export default function AboutMe(props) {
       >
         <div className="flex flex-row items-center ">
           <div className="flex flex-row  items-center mr-4">
-            <ArrowIcon className={"flex-none h-4 md:h-6 w-4 md:w-5 translate-y-[0.5px] text-AAsecondary"} />
-            <span className="text-AAsecondary font-Header text-sm  sm:text-xl"> 01.</span>
+            <ArrowIcon
+              className={
+                "flex-none h-4 md:h-6 w-4 md:w-5 translate-y-[0.5px] text-AAsecondary"
+              }
+            />
+            <span className="text-AAsecondary font-Header text-sm  sm:text-xl">
+              {" "}
+              01.
+            </span>
             <span className="flex-none text-gray-200 opacity-85 font-bold tracking-wider text-lg sm:text-2xl pl-4">
               About Me
             </span>
@@ -61,19 +74,17 @@ export default function AboutMe(props) {
           <div className="w-full md:w-7/12 space-y-4 sm:text-base text-sm ">
             <div className="font-Header ">
               <span className="text-gray-400 ">
-              {highlightText(
+                {highlightText(
                   "Hello! My name is Aditya, and I am a passionate software engineer and problem solver, dedicated to creating impactful codes that thrive on the internet. My journey in computer science began in 2022 when I delved into the world of full-stack development. Starting with the basics of HTML, CSS, and JavaScript, I quickly realized the potential of creating dynamic and responsive web applications. As I expanded my knowledge to include frameworks like React and backend technologies such as Node.js and MongoDB, I became passionate about building comprehensive web solutions. This marked the start of my journey as a full-stack developer, driven by a desire to continuously learn and innovate in the ever-evolving tech landscape."
                 )}
-
               </span>
             </div>
             <div className="font-Header ">
               <span className="text-gray-400 ">
-              {highlightText(
+                {highlightText(
                   "Since pursuing my engineering degree, my career has spanned diverse industries, allowing me to refine my expertise in both desktop and web development. I am passionate about creating innovative and user-friendly solutions using modern technologies. My experience includes working on impactful projects for Startup companies where I developed a comprehensive website to increase awareness of their services. With a dedication to continuous learning and improvement, I strive to deliver high-quality solutions that make a positive impact."
                 )}
               </span>
-
             </div>
             {/* <div className="font-Header tracking-wide">
               <span className="text-gray-400  ">
@@ -90,7 +101,8 @@ export default function AboutMe(props) {
 
             <div className="font-Header tracking-wide">
               <span className="text-gray-400  ">
-                Here are a few technologies I&apos;ve been working with recently :
+                Here are a few technologies I&apos;ve been working with recently
+                :
               </span>
             </div>
             <div className="font-Header tracking-wide flex flex-row space-x-16">
@@ -98,9 +110,14 @@ export default function AboutMe(props) {
                 <div className="flex flex-col space-y-4 sm:text-base text-sm">
                   {technologies[0].map((tech, index) => {
                     return (
-                      <div key={index} className="flex flex-row items-center space-x-2">
+                      <div
+                        key={index}
+                        className="flex flex-row items-center space-x-2"
+                      >
                         <ArrowIcon className={"h-3 w-3 text-AAsecondary"} />
-                        <span className="text-gray-400 sm:text-sm text-xs">{tech}</span>
+                        <span className="text-gray-400 sm:text-sm text-xs">
+                          {tech}
+                        </span>
                       </div>
                     );
                   })}
@@ -110,9 +127,14 @@ export default function AboutMe(props) {
                 <div className="flex flex-col space-y-4 sm:text-base text-sm">
                   {technologies[1].map((tech, index) => {
                     return (
-                      <div key={index} className="flex flex-row items-center space-x-2">
+                      <div
+                        key={index}
+                        className="flex flex-row items-center space-x-2"
+                      >
                         <ArrowIcon className={"h-3 w-3 text-AAsecondary"} />
-                        <span className="text-gray-400 sm:text-sm text-xs">{tech}</span>
+                        <span className="text-gray-400 sm:text-sm text-xs">
+                          {tech}
+                        </span>
                       </div>
                     );
                   })}
@@ -131,7 +153,7 @@ export default function AboutMe(props) {
             <div className="absolute w-5/6 h-5/6  rounded overflow-hidden">
               <div className="absolute w-full h-full group-hover:opacity-0 bg-AAsecondary opacity-10 duration-300 rounded overflow-hidden"></div>
               <Img
-                src={"/me.JPG"}
+                src={"/me.jpg"}
                 className={"object-contain rounded-lg"}
                 alt="My Image Not Found"
               />
